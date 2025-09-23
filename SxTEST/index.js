5f832e3da5934c2910fab45ec00ca8055074c9db
Revolvix <48681843+antonioclim@users.noreply.github.com>
2025-09-23 17:02:07 +0300

    Inițializare repo tw + subdirector SxTEST

---
+++ SxTEST/index.js
@@ -0,0 +1,4 @@
+﻿import express from "express";
+const app = express();
+app.get("/ping", (req, res) => res.send("pong"));
+app.listen(3000, () => console.log("SxTEST/minilearn pe http://localhost:3000"));
